682b6f86003a42efb98b44e886550c5ef7fa59cd
Torbilicious <torben.binder@googlemail.com>
2017-10-23 22:04:51 +0200

    Load friends via single call

--- src/main/kotlin/de/torbilicious/steam/api/WebApi.kt
+++ src/main/kotlin/de/torbilicious/steam/api/WebApi.kt
@@ -34,73 +34,69 @@ class WebApi {
     }
 
     fun loadId(id: Long): Pair<SteamId, SteamFriends> {
-
         @Suppress("UNNECESSARY_SAFE_CALL")
         if (id == steamId?.id) return Pair(steamId, friends)
 
         val friendListResponseString = getSteamResponse(SteamInterface.ISteamUser,
                 "GetFriendList",
                 mapOf("steamid" to id.toString()),
-                id,
                 1)
 
         val friendListResponse = gson.fromJson(friendListResponseString, FriendListResponse::class.java)
 
         val friendsDto = friendListResponse.friendslist.friends
 
-        //TODO: load friend data via call to "GetPlayerSummaries"
+        friends = loadExcludingFriends(friendsDto.map { it.steamid.toLong() })
 
-        println("Loading ${friendsDto.size} friends")
-        val deferred = friendsDto.map {
-            async(CommonPool) {
-                loadExcludingFriends(it.steamid.toLong())
-            }
-        }
-
-        steamId = loadExcludingFriends(id)
-
-        val friends = runBlocking {
-            deferred.map { it.await() }
-        }
-
-        this.friends = friends
+        steamId = loadExcludingFriends(listOf(id)).first()
 
         return Pair(steamId, friends)
     }
 
-    private fun loadExcludingFriends(id: Long): SteamId {
-        println("Loading id $id")
+    private fun loadExcludingFriends(ids: List<Long>): List<SteamId> {
+        println("Loading ${ids.size} ids")
 
-        val gamesResponseString = getSteamResponse(SteamInterface.IPlayerService,
-                "GetOwnedGames",
-                mapOf("include_appinfo" to "1"),
-                id)
+        val deferred = ids.map {
+            async(CommonPool) {
+                val gamesResponseString = getSteamResponse(SteamInterface.IPlayerService,
+                        "GetOwnedGames",
+                        mapOf("include_appinfo" to "1",
+                                "include_played_free_games" to "1",
+                                "steamid" to it.toString()))
 
-        val gamesResponse = gson.fromJson(gamesResponseString, GameResponse::class.java)
+                Pair(it, gson.fromJson(gamesResponseString, GameResponse::class.java))
+            }
+        }
+
+        val games = runBlocking {
+            deferred.map { it.await() }
+        }
 
         val summaryResponseString = getSteamResponse(SteamInterface.ISteamUser,
                 "GetPlayerSummaries",
-                mapOf("steamids" to id.toString()),
-                id,
+                mapOf("steamids" to ids.joinToString(",")),
                 2)
 
         val summaryResponse = gson.fromJson(summaryResponseString, IdSummaryResponse::class.java)
-        val player = summaryResponse.response.players.first()
 
-        return SteamId(id,
-                gamesResponse.response.games ?: listOf(),
-                player.personaname,
-                player.avatar)
+        return summaryResponse.response.players.map { player ->
+            SteamId(player.steamid.toLong(),
+                    games
+                            .find { it.first == player.steamid.toLong() }
+                            ?.second?.response?.games ?: listOf(),
+                    player.personaname,
+                    player.avatar)
+        }
+
     }
 
     private fun getSteamResponse(steamInterface: SteamInterface,
                                  method: String,
                                  parameters: Map<String, String>,
-                                 id: Long,
                                  version: Int = 1): String {
         val sb = StringBuilder()
 
-        sb.append("$baseUrl/$steamInterface/$method/v$version/?key=$apiKey&steamid=$id")
+        sb.append("$baseUrl/$steamInterface/$method/v$version/?key=$apiKey")
 
         parameters.forEach { k, v ->
             sb.append("&$k=$v")
@@ -134,7 +130,7 @@ data class Game(val appid: Number, val playtime_forever: String, val name: Strin
 data class IdSummaryResponse(val response: IdSummary)
 data class IdSummary(val players: PlayerSummaries)
 typealias PlayerSummaries = List<PlayerSummary>
-data class PlayerSummary(val personaname: String, val avatar: String)
+data class PlayerSummary(val personaname: String, val avatar: String, val steamid: String)
 
 data class FriendListResponse(val friendslist: Friendslist)
 data class Friendslist(val friends: Friends)
